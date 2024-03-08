55f2e4a49236875a673be893d4018f9e6c451f97
Ananay Mahajan <ananaymahajan2699@gmail.com>
2024-03-08 15:38:17 +0530

    updated the code

---
+++ src/customNodes/SideBar.js
@@ -0,0 +1,45 @@
+// Sidebar.js
+import React from "react";
+import TextUpdaterNode from "./TextUpdaterNode";
+import SortNode from "./SortNode";
+
+const NodeType = {
+  input: TextUpdaterNode,
+  sort: SortNode,
+};
+
+export default () => {
+  const onDragStart = (event, nodeType) => {
+    event.dataTransfer.setData("application/reactflow", nodeType);
+    event.dataTransfer.effectAllowed = "move";
+  };
+
+  return (
+    <aside>
+      <div className="description">
+        You can drag these nodes to the pane on the right.
+      </div>
+      <div
+        className="dndnode input"
+        onDragStart={(event) => onDragStart(event, "default")}
+        draggable
+      >
+        Input Node
+      </div>
+      <div
+        className="dndnode"
+        onDragStart={(event) => onDragStart(event, "default")}
+        draggable
+      >
+        Default Node
+      </div>
+      <div
+        className="dndnode output"
+        onDragStart={(event) => onDragStart(event, "default")}
+        draggable
+      >
+        Output Node
+      </div>
+    </aside>
+  );
+};
